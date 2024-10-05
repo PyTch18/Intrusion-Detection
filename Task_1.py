df422a76210243b76d3cd6b2b5a884a71ab54a0f
PyTch18 <amr2018azouz@gmail.com>
2024-10-06 01:23:07 +0300

    Task_part_1_Quartiles edited

--- Task_1.py
+++ Task_1.py
@@ -9,22 +9,27 @@ def calc_quartiles_stats(q_df):
     for column in q_df:
 
         try:
-            quartiles = pd.qcut(q_df[column], 4, duplicates='drop')
+            q_df[f'{column}_quartile'] = pd.qcut(q_df[column], 4, labels= False, duplicates='drop')
 
-            labels = [f'Q{i+1}' for i in range(len(quartiles.cat.categories))]
+            print(f"Statistics for {column}:")
+            for quartile in range(4):
 
-            q_df[f'f {column}_quartile'] = pd.qcut(q_df[column], len(labels),labels = labels, duplicates='drop')
+                quartile_data = q_df[q_df[f'{column}_quartile'] == quartile][column]
 
+                max_val = quartile_data.max()
+                min_val = quartile_data.min()
+                avg = quartile_data.mean()
+                var = quartile_data.var()
 
-            quartile_stats = q_df.groupby(f'f {column}_quartile')[column].agg(['max', 'min', 'mean', 'var'])
-            stats[column] = quartile_stats
+                print(f"Quartiles {quartile+1}")
+                print(f"Max: {max_val}")
+                print(f"Min: {min_val}")
+                print(f"Avg: {avg}")
+                print(f"Var: {var}")
 
-            print(f"\nStatistics for {column}:")
-            print(quartile_stats)
         except ValueError as e:
             print(f"Error processing {column}: {e}")
 
-    return stats
 
 df = pd.read_csv("Train_data.csv") #Data fields
 
@@ -43,7 +48,6 @@ Min_f = numeric_df.min() #numeric cuz getting the no numeric doesn't seem benefi
 Av_f = numeric_df.mean() #numeric to avoid errors
 Var_f = numeric_df.var() #numeric to avoid errors
 
-Q_stats = calc_quartiles_stats(numeric_df)
 
 
 print("Data Fields:\n", list(fields)) #1- a
@@ -54,5 +58,5 @@ print("Max Value: \n", Max_f) #1- e MAX
 print("Min Value: \n", Min_f) #1- e MIN
 print("Av Value:\n", Av_f) #1- e Average
 print("Var Value:\n", Var_f) #1- e Variance
-#print("Q Stats:\n", Q_stats) #1- f
+calc_quartiles_stats(numeric_df) #1- f Quartiles
 
